89581184a840f09d024c598e328842cf7f840253
Ayush-Shambharkar7 <ayushshambharkar0007@gmail.com>
2024-08-09 19:24:33 +0530

    Create simple_calculator.cpp

---
+++ simple_calculator.cpp
@@ -0,0 +1,29 @@
+//Name - Ayush Shambharkar
+//PRN - 23070123034
+//Aim - Simple calculator
+
+#include <iostream>
+using namespace std;
+
+int main() {   
+    float num1,num2;
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+    cout << "The sum is: " << num1+num2 << "\n";;
+    cout << "The difference is: " << num1-num2 << "\n";;
+    cout << "The division is: " << num1/num2 << "\n";;
+    cout << "The product is: " << num1*num2 << "\n";;
+
+    return 0;
+}
+/* 
+Output:
+Enter num1: 5
+Enter num2: 10
+The sum is: 15
+The difference is: -5
+The division is: 0.5
+The product is: 50
+*/
